64938b45af42c4293a149db5f33a010631a22116
Zak <zakariabouaddou@gmail.com>
2024-09-15 14:24:41 -0700

    changed logic for assigning largest value variable. however still encounter issues.

--- Actvity11Program.cpp
+++ Actvity11Program.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 	double first_var;
 	double second_var;
+	double largest_var;
 	
 	cout << "Enter a variable: " << endl;
 	cin >> first_var;
@@ -12,14 +13,14 @@ int main() {
 	cout << "Enter a second variable: " << endl;
 	cin >> second_var;
 
+
 	if (first_var > second_var)
-		double largest_value;
-		first_var = largest_var;
+		largest_var = first_var;
 	else if (second_var > first_var)
-		second_var = largest_var;
+		largest_var = second_var;
 	else
-		largest_var = 0;
 		cout << "There is no largest value as both variables given are equal.";
+		largest_var = 0;
 
 	cout << largest_var;
 
